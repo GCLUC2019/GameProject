307511eda8997b2d96c202aed05f232990058735
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-06 15:21:08 +0900

    直し

--- プログラム研修/Projects/nakao/BaseProject2017/GameProject/Ball/Ball.cpp
+++ プログラム研修/Projects/nakao/BaseProject2017/GameProject/Ball/Ball.cpp
@@ -13,16 +13,17 @@ CVector2D point_data[] = {
 	CVector2D(200,70),
 	CVector2D(500,60),
 	CVector2D(52,30),
-	CVector2D(120,50)
+	CVector2D(1220,50)
 };
 
 Ball::Ball() :Task(eId_Ball, eUp_Ball, eRd_Ball)
 {
-	//m_pos = point_data[Utility::Rand(0, 5)];
-	m_pos = CVector2D(Utility::Rand(0, 1280), Utility::Rand(0, 520));
-	int i = Utility::Rand(-10, 10);
+	m_pos = point_data[Utility::Rand(0, 5)];
+	//m_pos = CVector2D(Utility::Rand(0, 1280), Utility::Rand(0, 520));
+	/*int i = Utility::Rand(-10, 10);
 	int j = Utility::Rand(-50, -20);
-	m_vec = CVector2D(i, j);
+	m_vec = CVector2D(i, j);*/
+	m_vec = CVector2D(20, -20);
 }
 Ball::~Ball()
 {
