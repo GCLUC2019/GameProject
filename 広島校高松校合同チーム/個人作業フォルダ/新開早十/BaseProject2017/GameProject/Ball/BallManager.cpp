a2e5bd69456615dc1d4514e5a10be0931ae03dec
Shingai-Hayato-Takamatsu <hayatomsi@gmail.com>
2019-03-06 16:49:39 +0900

    テスト版開発用

---
+++ 広島校高松校合同チーム/個人作業フォルダ/新開早十/BaseProject2017/GameProject/Ball/BallManager.cpp
@@ -0,0 +1,35 @@
+#include "BallManager.h"
+#include "Ball.h"
+#include "../Global.h"
+BallManager::BallManager():Task(eId_Ball,eUp_Ball,eRd_Ball)
+{
+
+}
+
+BallManager::~BallManager()
+{
+}
+
+void BallManager::Update(float delta_time)
+{
+	//マウス左ボタンを押しっぱなしなら
+	if (CInput::GetState(0, CInput::eHold, CInput::eMouseL)) {
+		// 1/60秒加算
+		m_time += SPF;
+		// 0.1秒で一つ発生
+		if (m_time > 0.1f) {
+			m_time -= 0.1f;
+			//マウスの位置を取得
+			CVector2D pos = CInput::GetMousePoint();
+			//マウスの位置に玉を生成
+			//new Ball(pos);
+
+			//ボールを作成
+			new Ball();
+		}
+	}
+}
+
+void BallManager::Render()
+{
+}
