a2a0fc3aa009757bd6a820da374cadd4b8891ed0
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-06 15:16:15 +0900

    rnd

--- プログラム研修/Projects/nakao/BaseProject2017/GameProject/Ball/Ball.cpp
+++ プログラム研修/Projects/nakao/BaseProject2017/GameProject/Ball/Ball.cpp
@@ -3,15 +3,36 @@
 Ball::Ball(const CVector2D& pos):Task(eId_Ball,eUp_Ball,eRd_Ball)
 {
 	m_pos = pos;
-
+	int i = Utility::Rand(-10, 10);
+	int j = Utility::Rand(-50, -20);
+	m_vec = CVector2D(i, j);
 }
 
+CVector2D point_data[] = {
+	CVector2D(100,100),
+	CVector2D(200,70),
+	CVector2D(500,60),
+	CVector2D(52,30),
+	CVector2D(120,50)
+};
+
+Ball::Ball() :Task(eId_Ball, eUp_Ball, eRd_Ball)
+{
+	//m_pos = point_data[Utility::Rand(0, 5)];
+	m_pos = CVector2D(Utility::Rand(0, 1280), Utility::Rand(0, 520));
+	int i = Utility::Rand(-10, 10);
+	int j = Utility::Rand(-50, -20);
+	m_vec = CVector2D(i, j);
+}
 Ball::~Ball()
 {
 }
 
 void Ball::Update(float delta_time)
 {
+
+	m_vec.y += GRAVITY * SPF;
+	m_pos += m_vec;
 
 
 	if (m_pos.y > SCREEN_HEIGHT) SetKill();
