bfd42591acde0e01e6bba2324fba5765b8dfd9f1
Banjo-Ryota-Takamatsu <bancha1319@gmail.com>
2019-03-07 17:06:10 +0900

    add

--- プログラム研修/Projects/banjo/BaseProject2017/GameProject/Ball/Ball.cpp
+++ プログラム研修/Projects/banjo/BaseProject2017/GameProject/Ball/Ball.cpp
@@ -2,24 +2,27 @@
 #include "../Global.h"
 
 //出現位置の数
-#define POINT_MAX 6
+#define POINT_MAX 5
 
 CVector2D point_data[] = {
-	CVector2D(64,64),
-	CVector2D(64*3,64),
-	CVector2D(64*5,64),
-	CVector2D(64*7,64),
-	CVector2D(64*9,64),
-	CVector2D(64 * 11,64)
+	CVector2D(100,360),
+	CVector2D(100*3,360),
+	CVector2D(100*5,360),
+	CVector2D(100*7,360),
+	CVector2D(100*9,360),
 };
 
 const int point_data_size = ARRAY_SIZE(point_data);
 
 Ball::Ball() :Task(eId_Ball, eUp_Ball, eRd_Ball)
+/*
+初期化子リスト
+ここに書いたやつは初期化される
+*/
 {
 	int r = rand() % point_data_size;
-	static_assert(POINT_MAX == point_data_size, "要素数オーバー");
-	assert(r < point_data_size);
+	/*static_assert(POINT_MAX == point_data_size, "要素数オーバー");
+	assert(r < point_data_size);*/
 	//デバッグの時だけ有効
 	m_pos = point_data[r];
 	//DEBUG_DATA_NUM("%d番目の要素が選ばれました\n",r);
@@ -27,7 +30,7 @@ Ball::Ball() :Task(eId_Ball, eUp_Ball, eRd_Ball)
 }
 Ball::Ball(const CVector2D& pos):Task(eId_Ball,eUp_Ball,eRd_Ball)
 {
-	m_pos = pos;
+	m_pos = pos+CVector2D(50,0);
 	m_vec = CVector2D(0, -20);
 }
 
@@ -38,8 +41,8 @@ Ball::~Ball()
 void Ball::Update(float delta_time)
 {
 	//重力の落下速度
-	m_vec.y += GRAVITY * SPF;
-	m_pos += m_vec;
+	m_vec.y += GRAVITY * delta_time;
+	m_pos += m_vec * delta_time;
 	//if (m_pos.y > 660)m_vec = m_vec * -1;
 	if (m_pos.y > SCREEN_HEIGHT) SetKill();
 }
@@ -47,9 +50,4 @@ void Ball::Update(float delta_time)
 void Ball::Render()
 {
 	Utility::DrawQuad(m_pos, CVector2D(32.0f,32.0f), CVector4D(1.0f, 0, 0, 1));
-}
-
-void Ball::SetVector(const CVector2D &b)
-{
-	m_vec = b;
 }
