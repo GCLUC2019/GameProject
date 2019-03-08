ae41cabdedc28ab6d40dacce82fb44aa277db2a2
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-08 14:20:02 +0900

    a

--- プログラム研修/Projects/nakao/gravity/gravity/Task/TaskManager.h
+++ プログラム研修/Projects/nakao/gravity/gravity/Task/TaskManager.h
@@ -3,16 +3,18 @@
 
 class TaskManager {
 private:
-	static Task * m_head;	//先頭ポインタ
-	static Task * m_last;	//末尾ポインタ
-
+	static Task* m_head;	//先頭ポインタ
+	static Task* m_last;	//末尾ポインタ
+	static Task* m_exclude;	//除外リスト先頭ポインタ
 public:
 	TaskManager();
+	
 	/*!
 	@brief	先頭に接続
 	@param	Task* t			[in] 作成したオブジェクトのポインタ
 	**/
 	static void Addf(Task* t);
+	
 	/*!
 	@brief	末尾に接続
 	@param	Task* t			[in] 作成したオブジェクトのポインタ
@@ -24,11 +26,10 @@ public:
 	static Task* GetLast() {
 		return m_last;
 	}
-	/*!
-	@brief	末尾に接続
-	@param	bool f			[in]フラグ　tureなら昇順flseなら降順 
-	**/
-	static void TaskSort(bool f);
+	
+	static Task* GetExclude() {
+		return m_exclude;
+	}
 	
 	static void SetHead(Task* t) {
 		m_head = t;
@@ -37,15 +38,32 @@ public:
 		m_last = t;
 	}
 	/*!
+	@brief
+	@param	bool f			[in]フラグ　tureなら昇順flseなら降順
+	**/
+	static void TaskSort(bool f);
+	/*!
 	@brief	入れ替え関数　t1とt2を入れ替える
 	@param	Task* t1			[in] 入れ替えたいオブジェクトのポインタ
 	@param	Task* t2			[in] 入れ替えたいオブジェクトのポインタ
 	**/
 	static void Swap(Task* t1, Task* t2);
+	
+	/*!
+	@brief	リストから除外して除外リストに追加
+	@param	Task* t			[in] 除外するオブジェクトのポインタ
+	**/
+	static void Exclusion(Task* t);
+	
+	/*!
+	@brief	除外リストを空にする（リスト内全削除）
+	**/
+	static void  ExclusionDelete();
 	/*!
 	@brief	リスト全てのDraw関数を呼び出す
 	@param	Task* t			[in] オブジェクトのポインタ
 	@param	bool f			[in] フラグ　tureなら先頭からflseなら末尾から
 	**/
 	static void DrawAll(Task* t, bool f);
+
 };
