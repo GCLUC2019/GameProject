2e77d1d98b7bc395c6a8c73ff53f639b5a024a64
kunimi-yuuki-takamatsu <kunimi1925@gmail.com>
2019-03-08 09:28:38 +0900

    add

--- プログラム研修/Projects/kunimi/BaseProject2017/GameProject/Ball/Ball.cpp
+++ プログラム研修/Projects/kunimi/BaseProject2017/GameProject/Ball/Ball.cpp
@@ -3,11 +3,15 @@
 //出現位置の数
 #define POINT_MAX 5
 
-Ball::Ball(const CVector2D& pos):Task(eId_Ball,eUp_Ball,eRd_Ball)
-{
-	m_pos = pos;
-	m_vec = CVector2D(0, -20);
+Ball::Ball(const CVector2D& pos):Task(eId_Ball,eUp_Ball,eRd_Ball),
 
+/*	初期化子リスト
+ヘッダーファイルに定義した順番から初期化
+*/
+m_pos(pos),
+m_vec(CVector2D(0, -20))
+{
+	//安全面を考慮してポインタの受け渡しはこちらで行う
 }
 
 
@@ -45,8 +49,8 @@ void Ball::Update(float delta_time)
 	//DEBUG_PRINT("デバッグ中");
 
 	//Global.h参照
-	m_vec.y += GRAVITY * SPF;
-	m_pos += m_vec;
+	m_vec.y += GRAVITY * delta_time;
+	m_pos += m_vec * delta_time;
 
 	
 	if (m_pos.y > SCREEN_HEIGHT) SetKill();
@@ -57,7 +61,5 @@ void Ball::Render()
 	Utility::DrawQuad(m_pos, CVector2D(32.0f,32.0f), CVector4D(1.0f, 0, 0, 1));
 }
 
-void Ball::SetVector(const CVector2D & vec)
-{
-	m_vec = vec;
-}
+
+
