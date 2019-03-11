1e3fb851dd7ad79bc8a18f7cb6828e79046fcde0
kunimi-yuuki-takamatsu <kunimi1925@gmail.com>
2019-03-11 14:41:40 +0900

    update

--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/EnemyBase.h
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/EnemyBase.h
@@ -20,9 +20,11 @@ protected:
 	
 	CVector2D m_pos;	//敵位置
 	CVector2D m_vec;	//敵ベクトル
-	CImage m_img;		//敵画像
+	CAnimImage m_img;	//敵画像
+	CVector2D m_pos_old;//敵前位置
 	int m_type;			//敵識別子
 	int m_e_hp;			//敵体力
+	int m_state;		//敵状態
 	float m_ang;		//敵方向
 	
 public:
@@ -31,5 +33,9 @@ public:
 	virtual void Attack();
 	virtual void Update();
 	virtual void Draw();
+	/*
+	@drief オブジェクトの画面移動を制限する
+	*/
+	virtual void MoveControl();
 
 };
