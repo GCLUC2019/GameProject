cf5fab3ec808620e151e782dedcd18e22ed43cc8
Banjo-Ryota-Takamatsu <bancha1319@gmail.com>
2019-03-11 10:40:13 +0900

    add

---
+++ 高松チーム/Gameproject/GameProject/Boss.cpp
@@ -0,0 +1,14 @@
+#include "Boss.h"
+#include "../GameProject/Game/Resource/Resource.h"Å@
+
+BossHead::BossHead()
+{
+}
+
+void BossHead::Update()
+{
+}
+
+void BossHead::Draw()
+{
+}
---
+++ 高松チーム/Gameproject/GameProject/Boss.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../GameProject/Game/Character/EnemyBase/EnemyBase.h"
+
+
+class BossHead :public EnemyBase{
+protected:
+
+
+public:
+	BossHead();
+
+	void Update();
+
+	void Draw();
+
+};
