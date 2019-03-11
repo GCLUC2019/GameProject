ee1ac5d09ff8315ac37628b77ba9c63106e9b2df
kunimi-yuuki-takamatsu <kunimi1925@gmail.com>
2019-03-11 11:22:49 +0900

    update

--- 高松チーム/Gameproject/GameProject/Game/Scene/Result.cpp
+++ 高松チーム/Gameproject/GameProject/Game/Scene/Result.cpp
@@ -2,14 +2,21 @@
 
 GameClear::GameClear()
 {
-
-}
-
-void GameClear::Draw()
-{
+	m_gameclear_pos = CVector2D(1280 / 2, 720 / 2);
+	m_gameclear_img = COPY_RESOURCE("GameClear", CImage*);
+	
 }
 
 void GameClear::Update()
 {
-	Draw();
+	
 }
+
+void GameClear::Draw()
+{
+	m_gameclear_img.SetSize(300, 100);
+	m_gameclear_img.SetPos(m_gameclear_pos);
+	m_gameclear_img.Draw();
+}
+
+
