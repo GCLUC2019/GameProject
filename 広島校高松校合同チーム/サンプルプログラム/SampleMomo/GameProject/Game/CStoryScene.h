4c1a5a5f0d61802fc03aee140b0255c9ea659ec5
Matsukwa-Hroaki-Takamatsu <hiro18276779919@gmail.com>
2019-03-11 11:50:16 +0900

    カットシーン追加

--- 広島校高松校合同チーム/サンプルプログラム/SampleMomo/GameProject/Game/CStoryScene.h
+++ 広島校高松校合同チーム/サンプルプログラム/SampleMomo/GameProject/Game/CStoryScene.h
@@ -4,9 +4,15 @@
 
 class CStoryScene : public CObject {
 private:
+	CImage m_s_img;
+	CImage m_text_box;
+	CImage m_text;
 public:
 	CStoryScene();
 	~CStoryScene();
 	void Update();
 	void Draw();
+	void SetTextBox();
+	void SetStory(char story_name[]);
+	void SetText(char text_name[]);
 };
