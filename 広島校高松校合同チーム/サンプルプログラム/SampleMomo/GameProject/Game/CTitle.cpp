13c4d419e2969f0da77f9d781e901217de26b71e
Mino-Hakuro-Takamatsu <bocchi3064@gmail.com>
2019-03-11 15:48:31 +0900

    追加

--- 広島校高松校合同チーム/サンプルプログラム/SampleMomo/GameProject/Game/CTitle.cpp
+++ 広島校高松校合同チーム/サンプルプログラム/SampleMomo/GameProject/Game/CTitle.cpp
@@ -1,25 +1,42 @@
 #include "CTitle.h"
 #include "CObjectImage.h"
+#include "CStoryScene.h"
 CTitle::CTitle() : CObject(0, 0)
 {
 	ADD_RESOURCE("Back", CImage::LoadImage("Back.png"));
-	m_back = COPY_RESOURCE("Back", CImage*);
-	TaskManager::GetInstance()->AddTask(new CObjectImage(GET_RESOURCE("Back", CImage*), CVector3D(0, 0, 0), CVector2D(1280, 720), -1));
-	
+	m_background_image_p= GET_RESOURCE("Back", CImage*);
+	ADD_RESOURCE("Back_txt", CImage::LoadImage("Back_txt.png"));
+	m_backtxt_p = GET_RESOURCE("Back_txt", CImage*);
+	m_backtxt_p->SetPos(1050, -50);
 }
 
 CTitle::~CTitle()
 {
+	
 }
 
 void CTitle::Update()
 {
-
+	//スペースキーでタイトル画面を破棄
+	if (CInput::GetState(0,CInput::ePush, CInput::eButton1)) {
+		SetIsDelete();
+		//Delete();
+		next_scene();
+	}
 }
 
 void CTitle::Draw()
 {
-	m_back.Draw();
+	m_background_image_p->Draw();
+	m_backtxt_p->Draw();
+	//printf("Drawしました");
+}
+
+void CTitle::next_scene()
+{
+	CStoryScene*StoryScene_p = new CStoryScene();
+	TaskManager::GetInstance()->AddTask(StoryScene_p);
 }
 
 //更新履歴2019/03/11 タイトルを追加
+//ストーリーシーンのへ移行を追加
