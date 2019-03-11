807cbde8045be230f7a539b9f22875c52195212d
Matsukwa-Hroaki-Takamatsu <hiro18276779919@gmail.com>
2019-03-11 17:20:29 +0900

    システム追加

--- 広島校高松校合同チーム/サンプルプログラム/SampleMomo/GameProject/Game/CStoryScene.h
+++ 広島校高松校合同チーム/サンプルプログラム/SampleMomo/GameProject/Game/CStoryScene.h
@@ -7,18 +7,47 @@ private:
 	CImage m_s_img;
 	CImage m_text_box;
 	CImage m_text;
+	CImage m_icon;
 	int rect_cnt;
-	int dis_cnt = 0;
+	int dis_cnt;
+	int lim;
+	int sentence_num;
+	int draw_cnt;
+	int subscript;
+
+	struct rect_pos_size {
+		int top_x;
+		int top_y;
+		int bottom_x;
+		int bottom_y;
+		CVector2D size;
+		CVector2D pos;
+	};
+	
+	rect_pos_size pos_array[5];
+
+	bool complete_flg;
+
+	char name[1024];
+	char t_name[3][1024];
 public:
 	CStoryScene();
 	~CStoryScene();
 	void Update();
 	void Draw();
+	void SetParam();
 	void SetTextBox();
-	void SetStory(char story_name[]);
+	void SetIcon();
+	void SetStory(char story_name[], rect_pos_size pos_a);
 	void SetText(char text_name[]);
-	//表示したい文章の文字数
-	void UpdateText(int word);
+	void SetAll();
+	//次のテキストを表示する関数
+	void NextText(char text_name[][1024]);
+	//表示したい文章の文字数と文章数
+	void UpdateText(int word,int limit);
+	//アイコンを点滅させて表示する
+	void IconDraw();
+	void RectUp();
 };
 
 /*
