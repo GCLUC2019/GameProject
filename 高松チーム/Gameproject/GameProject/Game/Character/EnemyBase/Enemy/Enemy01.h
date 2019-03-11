f668bae1d782327293eed2561f67b01beda423d8
tanaka <yamadat609@gmail.com>
2019-03-11 14:43:11 +0900

    update

--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Enemy/Enemy01.h
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Enemy/Enemy01.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "../EnemyBase.h"
 #include "../../Player.h"
+#include "../GameProject/stdafx.h"
 /*
-田中　
+担当
+田中  class Enemy01　
 */
 
 class Enemy01: public EnemyBase{
@@ -16,12 +18,13 @@ private:
     int m_state;
     bool m_search_flg;
     bool m_move_dir_flg;
+
 public:
 	Enemy01();
 	void Update();
 	void Draw();
-    void Move();
-    void Search();
-    void Attack();
-    bool PlayerCheck(Task*p, Task*e);
+    void Move();//プレイヤー発見時の動き
+    void Search();//探索中の動き
+    void Attack();//攻撃
+    bool PlayerCheck(Player*p, Task*e,float _l);//プレイヤーを検知する
 };
