0197c784050a3990a7713ad221955413c8041aab
Banjo-Ryota-Takamatsu <bancha1319@gmail.com>
2019-03-11 11:37:55 +0900

    update

--- 高松チーム/Gameproject/GameProject/Boss.cpp
+++ 高松チーム/Gameproject/GameProject/Boss.cpp
@@ -3,7 +3,7 @@
 BossHead::BossHead():EnemyBase(eBossHead)
 {
 	m_img = COPY_RESOURCE("Boss", CImage*);
-	m_pos = CVector2D(100,100);
+	m_pos = CVector2D(1280-768,768-768/2);
 }
 
 void BossHead::Update()
@@ -13,7 +13,7 @@ void BossHead::Update()
 
 void BossHead::Draw()
 {
-	m_img.SetSize(768, 768);
+	m_img.SetSize(768/3,768/3);
 	m_img.SetRect(0,0,768,768);
 	m_img.SetPos(m_pos);
 	m_img.Draw();
