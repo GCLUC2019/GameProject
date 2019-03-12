4b5b7d7df44267d892ae99030e047d5b0b302009
kunimi-yuuki-takamatsu <kunimi1925@gmail.com>
2019-03-12 14:45:39 +0900

    update

--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Enemy/Enemy05.cpp
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Enemy/Enemy05.cpp
@@ -7,7 +7,7 @@
 
 #define MOVE_ 360
 
-Enemy05::Enemy05(CVector2D& pos) : EnemyBase(eEnemy05),
+Enemy05::Enemy05(CVector2D& _pos) : EnemyBase(eEnemy05),
 m_move_cnt(0)
 {
 	//èâä˙âª
@@ -15,7 +15,7 @@ m_move_cnt(0)
 	m_img.SetSize(IMAGE_SIZE, IMAGE_SIZE);
 	m_img.SetCenter(IMAGE_SIZE / 2, IMAGE_SIZE / 2);
 
-	m_pos = CVector2D(600, 100);
+	m_pos = _pos;
 	m_vec = CVector2D(0, 0);
 	m_hp = 100;
 	m_img.SetFlipH(m_flip);
--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/EnemyManager.cpp
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/EnemyManager.cpp
@@ -4,21 +4,72 @@
 #include "../EnemyBase/Enemy/Enemy05.h"
 
 EnemyManager::EnemyManager():
-	m_wave_flg(false)
+	m_wave_flg(false),
+	m_appear_type(eWave1_1),
+	m_wave1_1_flg(false),
+	m_wave1_2_flg(false),
+	m_wave2_1_flg(false),
+	m_wave2_2_flg(false)
 {
-	TaskManager::GetInstance()->AddTask(new Enemy01(CVector2D(Utility::Rand(0, 1280), Utility::Rand(400, 720))));
-	TaskManager::GetInstance()->AddTask(new Enemy02(CVector2D(Utility::Rand(0, 1280), Utility::Rand(400, 720))));
-	TaskManager::GetInstance()->AddTask(new Enemy05(CVector2D(Utility::Rand(0,1280), Utility::Rand(400, 720))));
+	
 
 }
 
 void EnemyManager::Update()
 {
-	
+	++m_cnt;
+	if (m_cnt >= 300)	m_appear_type = eWave1_2;
+	if (m_cnt >= 1000)	m_appear_type = eWave2_1;
+	if (m_cnt >= 1600)	m_appear_type = eWave2_2;
+
 }
 
 void EnemyManager::Draw()
 {
+	switch (m_appear_type) {
+	case eWave1_1:
+		if (m_wave1_1_flg == false) {
+			m_wave1_1_flg = true;
+			TaskManager::GetInstance()->AddTask(new Enemy01(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy02(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy05(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+		}
+		break;
+
+	case eWave1_2:
+		if (m_wave1_2_flg == false && m_wave1_1_flg == true) {
+			m_wave1_2_flg = true;
+			TaskManager::GetInstance()->AddTask(new Enemy01(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy02(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy01(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy02(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+		}
+		break;
+
+
+
+	case eWave2_1:
+		if (m_wave2_1_flg == false && m_wave1_2_flg == true) {
+			m_wave2_1_flg = true;
+			TaskManager::GetInstance()->AddTask(new Enemy05(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy05(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy05(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+		}
+		break;
+
+	case eWave2_2:
+		if (m_wave2_2_flg == false && m_wave2_1_flg == true) {
+			m_wave2_2_flg = true;
+			TaskManager::GetInstance()->AddTask(new Enemy01(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy02(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy01(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+			TaskManager::GetInstance()->AddTask(new Enemy05(CVector2D(Utility::Rand(0, 1280), Utility::Rand(360, 720))));
+		}
+		break;
+
+	default:
+		break;
+	}
 }
 
 
