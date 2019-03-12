d274bcdba8e7bb87fb1d8f7c2118b0bf377dc4be
Banjo-Ryota-Takamatsu <bancha1319@gmail.com>
2019-03-12 18:10:42 +0900

    Update

--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Boss/BossManager.h
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Boss/BossManager.h
@@ -7,6 +7,7 @@ private:
 		eNothing,
 		eIdle,
 		eAttack,
+		eDeath,
 	};
 
 	CAnimImage m_img; //頭
@@ -14,25 +15,36 @@ private:
 	CImage m_img3;//左手
 	CAnimImage m_img4;//尻尾
 
+	CImage m_img5;//やられたときの画像
+
 	CVector2D m_pos; //頭の座標
 	CVector2D m_pos2;//右手の座標
 	CVector2D m_pos3;//左手の座標
 	CVector2D m_pos4;//尻尾の座標
+	CVector2D m_pos5;//尻尾の座標
 	CVector2D m_center;
 	CVector2D m_center2;
 
-	int m_state;
+	int m_state;//ボスの状態
+	int m_boss_hp;//ボスの体力
 
 	float m_rot;//円周率
 	float m_rot2;//円周率
 	float m_r;  //半径
 
-	bool Idle_flag;
-	bool Attack_flag;
+	bool m_idle_flag;
+	bool m_attack_flag;
+	bool m_death_flag;
 
 public:
 
 	BossManager();
+
+	void Idle();
+
+	void Attack();
+
+	void Death();
 
 	void Update();
 
