6b5bf63f4533925ebed266da88e2589fcd98e35c
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-12 16:48:05 +0900

    a

--- 高松チーム/Gameproject/GameProject/Game/Item/Item.cpp
+++ 高松チーム/Gameproject/GameProject/Game/Item/Item.cpp
@@ -10,34 +10,31 @@ m_pos(_m_pos)
 	switch (m_item_type)
 	{
 	case eKoban:
-		m_img = COPY_RESOURCE("Kakera", CImage*);
-		//m_img.SetRect(50, 50, 100, 100);
+		m_img = COPY_RESOURCE("PEffectLongAttack", CImage*);
+		m_img.SetRect(0, 320, 32, 352);
 		m_img.SetPos(m_pos);
-		m_img.SetSize(30, 30);
-		m_img.SetCenter(15, 15);
+		m_img.SetSize(20, 20);
+		m_img.SetCenter(10, 10);
 		break;
 	case eInari:
-		//m_img = COPY_RESOURCE("PEffectLongAttack", CImage*);
-		
-		m_img = COPY_RESOURCE("Kakera", CImage*);
-		//m_img.SetRect(50, 50, 100, 100);
+		m_img = COPY_RESOURCE("PEffectLongAttack", CImage*);
+		m_img.SetRect(0, 256, 64, 320);
 		m_img.SetPos(m_pos);
-		m_img.SetSize(30, 30);
-		m_img.SetCenter(15, 15);
+		m_img.SetSize(40, 40);
+		m_img.SetCenter(20, 20);
 		break;
 	case eKakera:
 		m_img = COPY_RESOURCE("Kakera", CImage*);
-		//m_img.SetRect(50, 50, 100, 100);
 		m_img.SetPos(m_pos);
-		m_img.SetSize(30, 30);
-		m_img.SetCenter(15, 15);
+		m_img.SetSize(50, 50);
+		m_img.SetCenter(25, 25);
 		break;
 	case eHyoutan:
-		m_img = COPY_RESOURCE("Kakera", CImage*);
-		//m_img.SetRect(50, 50, 100, 100);
+		m_img = COPY_RESOURCE("PEffectLongAttack", CImage*);
+		m_img.SetRect(64, 256, 128, 320);
 		m_img.SetPos(m_pos);
-		m_img.SetSize(30, 30);
-		m_img.SetCenter(15, 15);
+		m_img.SetSize(40, 40);
+		m_img.SetCenter(20, 20);
 		break;
 	default:
 		break;
