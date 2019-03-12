a98da43eecde1e8f474d0b3050fb87badc28dfa0
Banjo-Ryota-Takamatsu <bancha1319@gmail.com>
2019-03-12 17:34:31 +0900

    Update

--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Boss/BossManager.h
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Boss/BossManager.h
@@ -3,6 +3,12 @@
 
 class BossManager : public Task {
 private:
+	enum {
+		eNothing,
+		eIdle,
+		eAttack,
+	};
+
 	CAnimImage m_img; //“ª
 	CImage m_img2;//‰Eè‹Ê‚ª‚ ‚é‚Ù‚¤
 	CImage m_img3;//¶è
@@ -12,8 +18,18 @@ private:
 	CVector2D m_pos2;//‰Eè‚ÌÀ•W
 	CVector2D m_pos3;//¶è‚ÌÀ•W
 	CVector2D m_pos4;//K”ö‚ÌÀ•W
+	CVector2D m_center;
+	CVector2D m_center2;
 
 	int m_state;
+
+	float m_rot;//‰~ü—¦
+	float m_rot2;//‰~ü—¦
+	float m_r;  //”¼Œa
+
+	bool Idle_flag;
+	bool Attack_flag;
+
 public:
 
 	BossManager();
