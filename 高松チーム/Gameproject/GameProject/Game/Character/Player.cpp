20539f67e6f86a02dfe09b9ba92c193085d13908
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-12 12:05:05 +0900

    a

--- 高松チーム/Gameproject/GameProject/Game/Character/Player.cpp
+++ 高松チーム/Gameproject/GameProject/Game/Character/Player.cpp
@@ -3,7 +3,7 @@
 #include "../Character/Anim/AnimDataPlayer.h"
 #include <stdio.h>
 #define GRAVITY -4//重力
-#define DEP_N 1200//奥行重石
+#define DEP_N 540//奥行重石
 #define JUMP_SPD 50
 
 enum PlayerState
@@ -28,9 +28,13 @@ m_squat_flg(false),
 m_attack_flg(false),
 m_jump_flg(false),
 m_flip(false),
+m_special_flg(false),
+m_damage_flg(false),
 m_jump_vec(0),
 m_state(eIdle),
-m_state_old(m_state)
+m_state_old(m_state),
+m_HP(100),
+m_special(0)
 {
 	m_pos = CVector2D(1280/2, 540);
 	m_img = COPY_RESOURCE("Player",CAnimImage*);
@@ -74,33 +78,30 @@ void Player::Move()
 	if (CInput::GetState(0, CInput::eHold, CInput::eUp)) {
 		m_pos.y -= m_speed;
 		m_depth = m_pos.y / DEP_N;
-		m_img.ChangeAnimation(ePRun);
 		if (m_jump_flg == false)
 			m_state = eMove;
 	}
 	if (CInput::GetState(0, CInput::eHold, CInput::eDown)) {
 		m_pos.y += m_speed;
 		m_depth = m_pos.y / DEP_N;
-		m_img.ChangeAnimation(ePRun);
 		if (m_jump_flg == false)
 			m_state = eMove;
 	}
 	if (CInput::GetState(0, CInput::eHold, CInput::eRight)) {
 		m_pos.x += m_speed;
 		m_flip = true;
-		m_img.ChangeAnimation(ePRun);
 		if (m_jump_flg == false)
 			m_state = eMove;
 	}
 	if (CInput::GetState(0, CInput::eHold, CInput::eLeft)) {
 		m_pos.x -= m_speed;
 		m_flip = false;
-		m_img.ChangeAnimation(ePRun);
 		if (m_jump_flg == false)
 			m_state = eMove;
 	}
 
-	
+	if (m_squat_flg == false && m_attack_flg == false && m_jump_flg == false && m_pos.x == m_pos_old.x&&m_pos.y == m_pos_old.y)
+		m_state = eIdle;
 }
 
 void Player::Jump()
@@ -216,6 +217,18 @@ void Player::Attack()
 	k++;
 }
 
+void Player::Damage(int _damage)
+{
+	if(m_damage_flg)
+		return;
+	if (m_HP <= 0) {
+		m_state = eDeath;
+		SetAnim();
+	}
+		
+	m_HP -= _damage;
+	m_damage_flg = true;
+}
 
 void Player::Special()
 {
@@ -275,24 +288,52 @@ void Player::SetAnim()
 
 void Player::Update()
 {
+#ifdef _DEBUG
+	if (CInput::GetState(0, CInput::eHold, CInput::eMouseL))
+		Damage(10);
+	if (CInput::GetState(0, CInput::eHold, CInput::eMouseR))
+		m_HP = 100;
+#endif // _DEBUG
+
+	if (m_HP < 0)
+		return;
+	m_img.SetColor(1, 1, 1, 1);
 	if (m_special_flg)
 		Special();
 	m_state_old = m_state;
 	m_pos_old = m_pos;
-	Move();
+	if (m_damage_flg)
+		DamageState();
+	else
+		Move();
 	if (m_attack_flg)
 		Attack();
 	if (m_pos.x < 0 || m_pos.x > 1280)
 		m_pos.x = m_pos_old.x;
 	if (m_pos.y < 720 / 2 || m_pos.y > 720)
 		m_pos.y = m_pos_old.y;
-	if (m_squat_flg == false && m_attack_flg == false && m_jump_flg == false&& m_pos.x == m_pos_old.x&&m_pos.y == m_pos_old.y)
-		m_state = eIdle;
+	
 	if (m_state != m_state_old)
 		SetAnim();
 	
 }
-
+void Player::DamageState()
+{
+	m_img.SetColor(0.5f, 0.5f, 0.5f, 1);
+	static int time = 60;
+	time--;
+	m_state = eDamage;
+	/*if (m_HP < 0) {
+		m_state = eDeath;
+		SetAnim();
+	}*/
+	if (time < 30)
+		Move();
+	if (time < 0) {
+		time = 60;
+		m_damage_flg = false;
+	}
+}
 void Player::Draw()
 {
 #ifdef _DEBUG
@@ -301,7 +342,7 @@ void Player::Draw()
 #endif // _DEBUG
 
 	
-#define SAIZE 200
+#define SAIZE 150
 	m_img.UpdateAnimation();
 	m_img.SetSize(SAIZE *m_depth, SAIZE *m_depth);
 	m_img.SetCenter(SAIZE * m_depth / 2, SAIZE * m_depth / 2);
--- 高松チーム/Gameproject/GameProject/Game/Character/Player.h
+++ 高松チーム/Gameproject/GameProject/Game/Character/Player.h
@@ -4,27 +4,35 @@
 作成　中尾	
 　操作　暫定
  十字　	:移動
- zキー	:しゃがみ	（画像が逆さ）
- xキー	:攻撃　　	（画像が横）
+ zキー	:しゃがみ	
+ xキー	:近距離攻撃　　	
  cキー	:ジャンプ
+ vキー	:遠距離攻撃
 */
 class Player : public CharacterBase {
 private:
-	CImage m_shadow;		//
 
+	int m_HP;				//体力
+	int m_special;			//必殺用変数（勾玉のやつ）
+
+	CImage m_shadow;		//
 	CVector2D m_pos_old;	//前座標
 	float m_speed;			//移動速度
 	int m_state;			// 状態
 	int m_state_old;		// 前状態
 	int m_jump_vec;			//ジャンプ値
 	float m_depth;			//描画奥行き
+
+
+	//フラグ
 	bool m_flip;			//反転フラグ
+	bool m_damage_flg;		//ダメージフラグ
 	bool m_jump_flg;		//ジャンプフラグ
 	bool m_squat_flg;		//しゃがみフラグ
 	bool m_attack_flg;		//攻撃フラグ
 	bool m_special_flg;		//必殺攻撃フラグ
-public:
-	Player();
+
+	
 	/*
 	@brief 移動制御関数
 	*/
@@ -45,8 +53,17 @@ public:
 	@brief アニメーション設定関数
 	*/
 	void SetAnim();
-
-
+	/*
+	@brief ダメージ状態制御関数（無敵とか死亡への移行とか）
+	*/
+	void DamageState();
+public:
+	/*
+	@brief 　			ダメージ関数
+	@param _damage	[in]与えるダメージ 
+	*/
+	void Damage(int _damage);
+	Player();
 	void Update();
 	void Draw();
 };
