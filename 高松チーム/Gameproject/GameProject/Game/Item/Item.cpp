246b22d09437fc7271e18f73410f5a7307b38ece
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-12 16:39:38 +0900

    a

---
+++ 高松チーム/Gameproject/GameProject/Game/Item/Item.cpp
@@ -0,0 +1,55 @@
+#include "Item.h"
+
+Item::Item(int _m_item_type, CVector2D _m_pos) : Task(eItem),
+m_item_type(_m_item_type),
+m_pos(_m_pos)
+{
+	/*m_item_type = _m_item_type;
+	m_pos = _m_pos;*/
+
+	switch (m_item_type)
+	{
+	case eKoban:
+		m_img = COPY_RESOURCE("Kakera", CImage*);
+		//m_img.SetRect(50, 50, 100, 100);
+		m_img.SetPos(m_pos);
+		m_img.SetSize(30, 30);
+		m_img.SetCenter(15, 15);
+		break;
+	case eInari:
+		//m_img = COPY_RESOURCE("PEffectLongAttack", CImage*);
+		
+		m_img = COPY_RESOURCE("Kakera", CImage*);
+		//m_img.SetRect(50, 50, 100, 100);
+		m_img.SetPos(m_pos);
+		m_img.SetSize(30, 30);
+		m_img.SetCenter(15, 15);
+		break;
+	case eKakera:
+		m_img = COPY_RESOURCE("Kakera", CImage*);
+		//m_img.SetRect(50, 50, 100, 100);
+		m_img.SetPos(m_pos);
+		m_img.SetSize(30, 30);
+		m_img.SetCenter(15, 15);
+		break;
+	case eHyoutan:
+		m_img = COPY_RESOURCE("Kakera", CImage*);
+		//m_img.SetRect(50, 50, 100, 100);
+		m_img.SetPos(m_pos);
+		m_img.SetSize(30, 30);
+		m_img.SetCenter(15, 15);
+		break;
+	default:
+		break;
+	}
+
+}
+
+void Item::Update()
+{
+}
+
+void Item::Draw()
+{
+	m_img.Draw();
+}
--- 高松チーム/Gameproject/GameProject/Game/Item/Item.h
+++ 高松チーム/Gameproject/GameProject/Game/Item/Item.h
@@ -1,1 +1,25 @@
 #pragma once
+#include"../../Base/Task.h"
+
+enum ItemList  
+{
+	eKoban,
+	eInari,
+	eKakera,
+	eHyoutan,
+};
+
+class Item : public Task {
+	
+	int m_item_type;
+	CImage m_img;
+	CVector2D m_pos;
+public:
+	/*
+	@brief 　			ダメージ関数
+	@param _damage	[in]与えるダメージ
+	*/
+	Item(int _m_item_type,CVector2D _m_pos);
+	void Update();
+	void Draw();
+};
