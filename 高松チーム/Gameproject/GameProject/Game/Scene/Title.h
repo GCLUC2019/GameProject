dcf0c73cfad1d56579b2eeb2f5283c3f940a3a87
tanaka <yamadat609@gmail.com>
2019-03-13 17:50:57 +0900

    add

--- 高松チーム/Gameproject/GameProject/Game/Scene/Title.h
+++ 高松チーム/Gameproject/GameProject/Game/Scene/Title.h
@@ -4,9 +4,19 @@
 
 class Title : public Task {
 private:
+    //画像データ
     CImage m_img_back;
-    CImage m_img_rogo;
-    CVector2D m_pos;
+    CImage m_img_rogobase;
+    CImage m_img_rogoclear;
+    CImage m_img_ohuda;
+    CImage m_img_pushstart;
+    //各位置
+    CVector2D m_title_pos;
+    CVector2D m_rogo_pos;
+    CVector2D m_ohuda_pos;
+    CVector2D m_start_pos;
+    int m_cnt;//カウント
+    float m_size;
 public:
     Title();
     void Update();
