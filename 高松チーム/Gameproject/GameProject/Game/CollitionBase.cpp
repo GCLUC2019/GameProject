283ce63bd41f308db79532171f884e4c3348b22d
Nakao-Masanori-takamatu <34246572427624s@gmail.com>
2019-03-14 12:30:26 +0900

    HIt

---
+++ 高松チーム/Gameproject/GameProject/Game/CollitionBase.cpp
@@ -0,0 +1,77 @@
+#include "CollitionBase.h"
+#include"../Game/Character/EnemyBase/EnemyBase.h"
+
+CollitionBase::CollitionBase()
+{
+}
+
+void CollitionBase::Update()
+{
+}
+
+bool CollitionBase::CollisionCheckRect(Task * b1, int _type)
+{
+	Task* b2 = TaskManager::GetInstance()->GetHead();
+	while (b2) {
+		if (b2->GetType() == _type)
+		{
+			/*switch (_type)
+			{
+			case eEnemy01:
+				b1 =dynamic_cast<EnemyBase>
+			default:
+				break;
+			}*/
+			CRect r1 = CRect(
+				b1->GetPos().x + b1->GetRect().m_left,
+				b1->GetPos().y + b1->GetRect().m_top,
+				b1->GetPos().x + b1->GetRect().m_right,
+				b1->GetPos().y + b1->GetRect().m_bottom);
+		
+			CRect r2 = CRect(
+				b2->GetPos().x + b2->GetRect().m_left,
+				b2->GetPos().y + b2->GetRect().m_top,
+				b2->GetPos().x + b2->GetRect().m_right,
+				b2->GetPos().y + b2->GetRect().m_bottom);
+			if (CCollision::CollisionRect(r1, r2))
+				return true;
+			else b2 = b2->GetNext();
+		}
+		else
+			b2 = b2->GetNext();
+	}
+
+	return false;
+	
+}
+
+bool CollitionBase::CollitionCheckflip(int fliptype, Task * b1, Task * b2)
+{
+	/*switch (fliptype)
+	{
+	case 0:
+		if (b1->m_pos.x < b2->m_pos.x) return true;
+		else return false;
+		break;
+
+	case 1:
+		if (b1->m_pos.x > b2->m_pos.x) return true;
+		else return false;
+		break;
+
+	case 2:
+		if (b1->m_pos.y > b2->m_pos.y) return true;
+		else return false;
+		break;
+
+	case 3:
+		if (b1->m_pos.y < b2->m_pos.y) return true;
+		else return false;
+		break;
+
+	default:
+		break;
+	}
+	*/
+	return false;
+}
