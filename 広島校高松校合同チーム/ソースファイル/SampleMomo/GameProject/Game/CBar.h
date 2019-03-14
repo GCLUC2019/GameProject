7b33a060d378ef9525e49e07bc8f828279f6633c
Mino-Hakuro-Takamatsu <bocchi3064@gmail.com>
2019-03-14 15:51:14 +0900

    追加

--- 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CBar.h
+++ 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CBar.h
@@ -6,13 +6,17 @@ class CBar: public CObject {
 private:
 	CImage * m_HPbar_img = nullptr;
 public:
-	int damage;
+	//int damage;
+	//int size_y;
 	float hp;
-	float now_hp;
-	int max_bar_size;
-	int now_bar_size;
-	int m_decrease_hp;
-	CBar();
+	float*now_hp;
+	float m_bar_value_now;
+	//int max_bar_size;
+	//int now_bar_size;
+	CVector2D m_bar_size;
+	CVector2D m_bar_pos;
+	float m_mult_hp;
+	CBar(CImage* _img, float*_hp,float _max_hp, CVector2D _pos, CVector2D _size);
 	~CBar();
 	void Update();
 	void Draw();
