324898e00dd23e32a2c5d7f022c837a3f7d348e3
kumadaakira <18010504@anabuki.net.ne.jp>
2019-03-14 12:15:47 +0900

    a

--- Fukuyama/福山チーム/セーサク/GameProgramming/CPlayerTank.h
+++ Fukuyama/福山チーム/セーサク/GameProgramming/CPlayerTank.h
@@ -1,19 +1,18 @@
 #ifndef CPLAYERTANK_H
 #define CPLAYERTANK_H
-#include "CTank.h"
-#include "CHpBar.h"
-
-extern CTexture Texture;
+#include"CTank.h"
+#include"CHpBar.h"
 
 class CPlayerTank :public CTank{
 public:
 	void Init();
 	void Update();
-	void Render();
-	CBoxCollider*mpBoxCollider;
-	int mFireInterver;
 	void OnCollision(CCollider*p);
+	CBoxCollider*mpBoxCollider;
+	int mFireInterval;
 	CHpBar mHpBar;
-};
 
+	void Render();
+
+};
 #endif
