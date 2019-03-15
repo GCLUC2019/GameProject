3f8989eaff4f96eeb29147992b5ef3a0ce4dedea
Maeoka-Hiroki-Fukuyama <18010512@anabuki@-net.ne.jp>
2019-03-15 11:04:52 +0900

    画像

--- Fukuyama/福山チーム/セーサク/GameProgramming/CMain.cpp
+++ Fukuyama/福山チーム/セーサク/GameProgramming/CMain.cpp
@@ -11,11 +11,12 @@
 #include "CSceneGame.h"
 #include "CBoss.h"
 #include "CTexture.h"
-
+#include"CSceneManager.h"
 CTexture Texture;
 CRectangle*Explosion;
 
 CScene*mpScene;
+CMap*mpMap;
 
 CPlayerTank *Tank;
 
@@ -26,18 +27,45 @@ CBoss *Boss = NULL;
 CEnemy1*Enemy1;           //追加　釜田
 CEnemy2*Enemy2;
 
-CScene::ESceneTag CMain::mSceneTag = CScene::EROOT;
+//CScene::ESceneTag CMain::mSceneTag = CScene::EROOT;
 
+CMap::EScneMap CMain::mSceneMap = CMap::EStat; //変更　前岡
 //初めに1回だけ実行する処理の定義
 void CMain::Init() {
-	mSceneTag = CScene::EGAME;
+	mSceneMap = CMap::EStat;
+	mpMap = new CMap();
+	mpMap->Init();
+
+	/*mSceneTag = CScene::EGAME;
 	mpScene = new CSceneGame();
-	mpScene->Init();
+	mpScene->Init();*/
 }
 
 //繰り返し実行する処理の定義
 void CMain::Update() {
-	mpScene->Update();
+
+	mpMap->Update();
+	if (mpMap->mScneMap != mSceneMap){
+		delete mpMap;
+		mpMap = 0;
+		switch (mSceneMap){
+		case CMap::EMap1:
+			mpMap = new CMap1();
+			mpMap->Init();
+			break;
+		case CMap::EMap2:
+			mpMap = new CMap2();
+			mpMap->Init();
+			break;
+		case CMap::EMap3:
+			mpMap = new CMap3();
+			mpMap->Init();
+			break;
+		}
+		
+	}
+
+	/*mpScene->Update();
 	if (mpScene->mSceneTag != mSceneTag){
 		delete mpScene;
 		mpScene = 0;
@@ -55,6 +83,6 @@ void CMain::Update() {
 			mpScene->Init();
 			break;
 		}
-	}
+	}*/
 }
 
