03553aa2956b5a9a58cd76c38d1a82e4864946aa
Maeoka-Hiroki-Fukuyama <18010512@anabuki@-net.ne.jp>
2019-03-15 10:09:36 +0900

    画像データ

--- Fukuyama/福山チーム/セーサク/GameProgramming/CSceneManager.cpp
+++ Fukuyama/福山チーム/セーサク/GameProgramming/CSceneManager.cpp
@@ -5,5 +5,12 @@
 void CStat::Init(){
 
 	Texture.Load("1eria.tga");
+	mStat.SetVertex(200, -200, 100, -100);
+	mStat.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+	mStat.SetTexture(&Texture, 0, 358, 142, 0);
+}
 
+void CStat::Update(){
+	mStat.Update();
+	mStat.Render();
 }
