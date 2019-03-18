0fc54acd13d27af32418174b3cdccf20d3022d2a
Horikawa-Kei-Takamatsu <krpn.gk@gmail.com>
2019-03-18 15:22:32 +0900

    サブ武器アイテムの基盤を作成しました

--- 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CSubWeapon.cpp
+++ 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CSubWeapon.cpp
@@ -1,13 +1,16 @@
 #include "../Game/CSubWeapon.h"
+#include "CCharacterPlayer.h"
+
+bool CSubWeaponItem::m_get_flag = true;
 
 CSubWeaponItem::CSubWeaponItem(CVector3D pos, int weapon_num) :CGameSceneObject(eTaskIdDropItem, 0)
 {
 	
-	m_equip_flag = false;
+	//m_equip_flag = false;
 	m_weapon_num = weapon_num;
 
 	SetPos(pos);
-	SetSize(CVector2D(500,500));
+	SetSize(CVector2D(150,150));
 
 	m_speed = 1.0f;
 	m_energy = 100.0f;
@@ -17,25 +20,26 @@ CSubWeaponItem::CSubWeaponItem(CVector3D pos, int weapon_num) :CGameSceneObject(
 	SetIsShowShadow(true);
 	SetRads(75, 150, 10);
 
-	SetShadowSize(CVector2D(160, 50));
-	SetDrawAdjPos(CVector2D(-60, 0.0f));
+	SetShadowSize(CVector2D(60, 15));
+	SetDrawAdjPos(CVector2D(0, -375.0f));
 
 	//当たり判定の優先度
 	SetCollisionPriority(5);
 
 	m_anim_p->SetWillPlayAnim(eItemAnimIdIdle);
 
+	m_target_p = CCharacterPlayer::GetInstance();
 }
 
 CSubWeaponItem::~CSubWeaponItem()
 {
-	
+	m_get_flag = true;
 
 }
 
 void CSubWeaponItem::LoadAnimImage()
 {
-	m_anim_p->SetAnimImage(eItemAnimIdle1, GET_RESOURCE("SpearIcon", CImage*));
+	m_anim_p->SetAnimImage(eItemAnimIdle1, GET_RESOURCE("IconSpear", CImage*));
 	//m_anim_image_p[eItemAnimIdle1] = GET_RESOURCE("Enemy_Damage_0", CImage*);
 
 	AnimInfo* m_anim_info = m_anim_p->GetAnimInfoArray();
@@ -47,9 +51,24 @@ void CSubWeaponItem::LoadAnimImage()
 
 void CSubWeaponItem::GameSceneObjectUpdate()
 {
-	m_anim_p->SetWillPlayAnim(eItemAnimIdIdle);
+	//m_anim_p->SetWillPlayAnim(eItemAnimIdIdle);
+
+	if (CInput::GetState(0, CInput::ePush, CInput::eButton4) ){
+		CVector3D player_pos = m_target_p->GetPos();
+		float x = m_pos.x - player_pos.x;
+		float z = m_pos.z - player_pos.z;
+		
+		if (m_get_flag && abs(x) < 100 && abs(z) < 50) {	//プレイヤーとの距離がX100Z50以内にいる場合拾えます
+			m_get_flag = false;
+			PlayerGetItem();
+			SetIsDelete();
+		}
+	}
 }
 
+//ここでプレイヤーに武器の種類と残り使用回数を与えてやると良いと思います
 void CSubWeaponItem::PlayerGetItem()
 {
+	m_get_flag = false;
+
 }
--- 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CSubWeapon.h
+++ 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CSubWeapon.h
@@ -3,6 +3,8 @@
 #include "CAnimation.h"
 #include "../Global.h"
 
+class CCharacterPlayer;
+
 enum {	//アニメーションの種類の番号
 	eItemAnimIdIdle,
 	eItemyAnimIdMax,
@@ -21,15 +23,21 @@ enum {  //サブ武器のタイプ
 
 class CSubWeaponItem : public CGameSceneObject {
 private:
-	bool m_equip_flag;		//武器を装備しているかのフラグ
+	
+	//bool m_equip_flag;		//プレイヤーがこの武器を装備しているかのフラグ
 	int m_weapon_num;		//武器の種類
 	float m_energy;			//武器の耐久値
+	
+	CCharacterPlayer* m_target_p;	//プレイヤーのポインタ
+
+	static bool m_get_flag;	//武器を拾えるかどうかのフラグ(重なった武器を同時に拾えないようにするフラグ)
 public:
 	CSubWeaponItem(CVector3D pos, int weapon_num);
 	~CSubWeaponItem();
-	void LoadAnimImage();
 
-	void GameSceneObjectUpdate();
+	void LoadAnimImage();			//アニメーション読み込み
+
+	void GameSceneObjectUpdate();	//更新処理
 
 	void PlayerGetItem();	//プレイヤーに拾われた時に呼ばれる
 
