2eb467d81df8d90506a5202ee09f817a22d3db06
Banjo-Ryota-Takamatsu <bancha1319@gmail.com>
2019-03-18 10:13:26 +0900

    delete

--- 高松チーム/Gameproject/GameProject/Game/GameData/GameData.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include"../../stdafx.h"
- struct GameData
-{
-	 CVector2D m_scroll;
-	 int m_score;
-};
-extern GameData g_game_data;
--- 高松チーム/Gameproject/GameProject/Game/Scene/GameScene.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../GameProject/Base/Task.h"
-
-class GameScene : public Task {
-private:
-public:
-	GameScene();
-	~GameScene();
-};
