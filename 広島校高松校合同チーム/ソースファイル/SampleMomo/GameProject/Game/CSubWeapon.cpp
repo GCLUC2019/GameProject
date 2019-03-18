31e06cee15298a74749b0c6449a241e05758495a
Horikawa-Kei-Takamatsu <krpn.gk@gmail.com>
2019-03-18 11:25:15 +0900

    ヘッダーとcppを追加しました

---
+++ 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CSubWeapon.cpp
@@ -0,0 +1,14 @@
+#include "../Game/CSubWeapon.h"
+
+CSubWeaponItem::CSubWeaponItem(CVector3D pos, int weapon_num)
+{
+
+}
+
+CSubWeaponItem::~CSubWeaponItem()
+{
+}
+
+void CSubWeaponItem::PlayerGetItem()
+{
+}
