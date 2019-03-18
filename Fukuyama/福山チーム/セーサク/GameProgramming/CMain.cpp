0972a54c303123ec4366207dd2581d1bd52f2c71
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2019-03-18 11:03:01 +0900

    1102

--- Fukuyama/福山チーム/セーサク/GameProgramming/CMain.cpp
+++ Fukuyama/福山チーム/セーサク/GameProgramming/CMain.cpp
@@ -14,11 +14,13 @@
 
 
 
-#include"CSceneManager.h"
+//#include"CSceneManager.h"  //前岡
 
 #include"CSpeedEnemy.h"
 
+CRectangle BackImage;
 CTexture Texture;
+CTexture MapTexture;
 CRectangle*Explosion;
 
 CScene*mpScene;
@@ -37,43 +39,49 @@ CEnemy2*Enemy2;
 
 CScene::ESceneTag CMain::mSceneTag = CScene::EROOT;
 
-CMap::EScneMap CMain::mSceneMap = CMap::EStat; //変更　前岡
+//CMap::EScneMap CMain::mMapTag = CMap::EMap1; //変更　前岡
 //初めに1回だけ実行する処理の定義
 void CMain::Init() {
-	mSceneMap = CMap::EStat;
+	/*mMapTag = CMap::EStat;  //前岡
 	mpMap = new CMap();
-	mpMap->Init();
+	mpMap->Init();*/
+	MapTexture.Load("1eria.tga");
 
-	/*mSceneTag = CScene::EGAME;
+	mSceneTag = CScene::EGAME;
 	mpScene = new CSceneGame();
-	mpScene->Init();*/
+	mpScene->Init();
 }
 
 //繰り返し実行する処理の定義
 void CMain::Update() {
-
-	mpMap->Update();
-	if (mpMap->mScneMap != mSceneMap){
-		delete mpMap;
-		mpMap = 0;
-		switch (mSceneMap){
-		case CMap::EMap1:
-			mpMap = new CMap1();
-			mpMap->Init();
-			break;
-		case CMap::EMap2:
-			mpMap = new CMap2();
-			mpMap->Init();
-			break;
-		case CMap::EMap3:
-			mpMap = new CMap3();
-			mpMap->Init();
-			break;
-		}
-		
+	BackImage.Draw(MapTexture, 0, 1000, 0, 750);
+	BackImage.x = 0;
+	BackImage.y = 0;
+	BackImage.h = 300;
+	BackImage.w = 400;
+	BackImage.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+	/*mpMap->Update();  //前岡
+	if (mpMap->mScneMap != mMapTag){
+	delete mpMap;
+	mpMap = 0;
+	switch (mMapTag){
+	case CMap::EMap1:
+	mpMap = new CMap1();
+	mpMap->Init();
+	break;
+	case CMap::EMap2:
+	mpMap = new CMap2();
+	mpMap->Init();
+	break;
+	case CMap::EMap3:
+	mpMap = new CMap3();
+	mpMap->Init();
+	break;
 	}
 
-	/*mpScene->Update();
+	}*/
+
+	mpScene->Update();
 	if (mpScene->mSceneTag != mSceneTag){
 		delete mpScene;
 		mpScene = 0;
@@ -90,7 +98,22 @@ void CMain::Update() {
 			mpScene = new CSceneLose();
 			mpScene->Init();
 			break;
+			//前岡
+			/*case CMap::EMap1:
+			mpMap = new CMap1();
+			mpMap->Init();
+			break;
+			case CMap::EMap2:
+			mpMap = new CMap2();
+			mpMap->Init();
+			break;
+			case CMap::EMap3:
+			mpMap = new CMap3();
+			mpMap->Init();
+			break;*/
 		}
-	}*/
+	}
+
+
 }
 
