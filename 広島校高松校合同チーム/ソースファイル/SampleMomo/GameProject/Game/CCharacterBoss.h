972ad2e82e444fcb310a8dc48ae02305864c7f44
Matsukwa-Hroaki-Takamatsu <hiro18276779919@gmail.com>
2019-03-18 10:19:26 +0900

    追加

--- 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CCharacterBoss.h
+++ 広島校高松校合同チーム/ソースファイル/SampleMomo/GameProject/Game/CCharacterBoss.h
@@ -2,6 +2,35 @@
 #include "CCharacterPlayer.h"
 #include "../Global.h"
 
+//起動誤差
+#define RANGE (150)
+
+//攻撃の適正距離の初期値
+#define DEF_JUST_DIST 350
+#define JUST_DIST_Z 25
+
+//次の適正距離
+#define NEXT_JUST_DIST1 400
+#define NEXT_JUST_DIST2 100
+
+//プレイヤーとの距離
+#define PLAYER_DIST＿FAR 600
+#define AWAY_DIST 500
+
+//各状態の限界時間
+#define IDLE_LIMIT 120
+#define WALK_LIMIT 180
+#define RUN_LIMIT  75
+
+//速度
+#define DEF_SPEED 1.5
+#define RUN_SPEED 4.5
+
+//攻撃範囲
+#define ATTACK1_RANGE_X 1.5
+#define RUN_SPEED 4.5
+
+
 //ボスのアニメーションの種類番号
 enum {
 	eEnemyAnimBossIdIdle = 0,
@@ -29,12 +58,27 @@ class CCharacterBoss :public CCharacter
 private:
 	enum {
 		eEnemyBossStateIdle=0,
+		eEnemyBossStateWalk,
 		eEnemyBossStateRun,
+		eEnemyBossStateAttack,
+		eEnemyBossStateAway,
 	};
+
+	struct boss_mode_counts {
+		int boss_idle = 0;
+		int boss_walk;
+		int boss_run = 0;
+	};
+
+	boss_mode_counts s_boss_mode;
 
 	CCharacterPlayer* m_player_p;
 
-	int m_boss_state;
+	int m_boss_state = eEnemyBossStateIdle;
+	int m_befor_state = m_boss_state;
+	int m_just_dist = DEF_JUST_DIST;
+	bool m_is_attack = true;
+	bool m_is_hit = false;
 	CVector3D m_player_pos = CVector3D(0, 0, 0);
 public:
 	CCharacterBoss();
@@ -43,9 +87,29 @@ public:
 	void CharacterDraw();
 
 	void LoadAnimImage();
+	void ChangeFlip();
+	//エネミーの状態をカウントする関数
+	void ModeCount();
+	//エネミーの移動距離を変更
+	void ChangeDist();
+	//エネミーの状態を変更
+	void ChangeState();
+	//以下、エネミーの行動状態
+	//移動系
 	void Idle();
 	void Run();
+	void Walk();
+	void Away();
+	void CloseToPlayer();
+	//攻撃系
+	void Attack1();
+	void Attack2();
+	void AttackHub();
+	//vecの加算
 	void Move();
+	void MoveLimit();
+	//距離の精査
+	void CheckDist();
 
 	void CharacterBeforeCollisionCheck();
 };
