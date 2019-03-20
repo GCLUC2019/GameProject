5b3e406d1a17d73b7f12aec012da72ef06e3b121
kunimi-yuuki-takamatsu <kunimi1925@gmail.com>
2019-03-20 14:30:49 +0900

    delete

--- 高松チーム/Gameproject/GameProject/Game/Character/Anim/AnimEnemyEffect.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../Gameproject/GameProject/stdafx.h"
-
-enum E2EffectAnim {
-    //Enemy02
-    eE2Attack,
-};
-extern TexAnimData E2AttackEffectAnimData[];
--- 高松チーム/Gameproject/GameProject/Game/Resource/Sound/Sound.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../GameProject/Base/Task.h"
-
-class Sound : public Task {
-private:
-public:
-	Sound();
-	void Update();
-};
