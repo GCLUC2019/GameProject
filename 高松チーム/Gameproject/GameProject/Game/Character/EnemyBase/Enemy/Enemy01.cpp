805c23ac07c55301acca39112c4b084c1daa40b3
tanaka <yamadat609@gmail.com>
2019-03-22 12:51:13 +0900

    update

--- 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Enemy/Enemy01.cpp
+++ 高松チーム/Gameproject/GameProject/Game/Character/EnemyBase/Enemy/Enemy01.cpp
@@ -8,7 +8,6 @@
 #define DEP_N 1200
 
 Enemy01::Enemy01() : EnemyBase(CharacterData::eEnemy01),
-m_state(eSearch),
 m_hover(0.0f),
 m_search_flg (false)
 {
@@ -21,11 +20,11 @@ m_search_flg (false)
     m_pos = CVector2D(200, 200);
     m_vec = CVector2D(0, 0);
     m_dir = CVector2D(0, 0);
+    m_state=eSearch;
     m_depth = (m_pos.y - DEP_N) / 3.5;
 }
 
 Enemy01::Enemy01(CVector2D & _pos) : EnemyBase(eEnemy01),
-m_state(eSearch),
 m_hover(0.0f),
 m_search_flg(false)
 {
@@ -36,10 +35,11 @@ m_search_flg(false)
     m_img.SetFlipH(m_flip);
     m_img.ChangeAnimation(eEMove01);
     m_pos = _pos;
+    m_state=eSearch;
 	m_hp = 100;
     m_vec = CVector2D(0, 0);
     m_dir = CVector2D(0, 0);
-	m_rect = CRect(-IMAGE_SIZE / 3.5f, -IMAGE_SIZE / 5.0f, IMAGE_SIZE / 3.5f, IMAGE_SIZE / 5.0f);
+    m_rect = CRect(-IMAGE_SIZE / 2, -IMAGE_SIZE / 2, IMAGE_SIZE / 2, IMAGE_SIZE / 2);
     m_depth = (m_pos.y - DEP_N) / 3.5;
 }
 
@@ -79,26 +79,25 @@ void Enemy01::Update()
 
 void Enemy01::Draw()
 {
-//#ifdef _DEBUG//デバッグ表示　見れない場合は背景をなくしてください
-//    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_left, m_pos.y + m_rect.m_top), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
-//    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_left, m_pos.y + m_rect.m_bottom), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
-//    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_right, m_pos.y + m_rect.m_top), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
-//    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_right, m_pos.y + m_rect.m_bottom), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
-//
-//#endif//DEBUG
+#ifdef _DEBUG//デバッグ表示　見れない場合は背景をなくしてください
+    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_left, m_pos.y + m_rect.m_top), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
+    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_left, m_pos.y + m_rect.m_bottom), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
+    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_right, m_pos.y + m_rect.m_top), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
+    Utility::DrawQuad(CVector2D(m_pos.x + m_rect.m_right, m_pos.y + m_rect.m_bottom), CVector2D(4, 4), CVector4D(1, 0, 0, 1));
+    Utility::DrawQuad(CVector2D(m_pos.x , m_pos.y ), CVector2D(4, 4), CVector4D(1, 1, 0, 1));
+
+#endif//DEBUG
     m_shadow.SetSize(SAIZE_SD + m_depth / 5, 50);
     m_shadow.SetCenter((SAIZE_SD + m_depth / 5) / 2, 50 / 2);
-    m_shadow.SetPos(CVector2D(m_pos.x, m_pos.y - g_game_data.m_scroll.y / 3 + IMAGE_SIZE/2));
+    m_shadow.SetPos(CVector2D(m_pos.x, m_pos.y - g_game_data.m_scroll.y / 3 ));
     m_shadow.Draw();
 
     m_hover += 0.1f;//リセットしたほうがいい？
 	m_img.SetSize(IMAGE_SIZE, IMAGE_SIZE);
     m_img.SetCenter(IMAGE_SIZE / 2, IMAGE_SIZE / 2);
-	m_img.SetRect(-IMAGE_SIZE, -IMAGE_SIZE - g_game_data.m_scroll.y / 3,IMAGE_SIZE, IMAGE_SIZE - g_game_data.m_scroll.y / 3);
-    m_img.SetPos(CVector2D(m_pos.x, m_pos.y + sin(m_hover)*10.0f - g_game_data.m_scroll.y / 3));
+    m_img.SetPos(CVector2D(m_pos.x, m_pos.y + sin(m_hover)*10.0f - g_game_data.m_scroll.y / 3 - IMAGE_SIZE / 2));
     m_img.SetFlipH(m_flip);
-	m_rect = CRect(-IMAGE_SIZE / 3.5f, -IMAGE_SIZE / 5.0f - g_game_data.m_scroll.y / 3, 
-					IMAGE_SIZE / 3.5f, IMAGE_SIZE / 5.0f - g_game_data.m_scroll.y / 3);
+    m_rect = CRect(-IMAGE_SIZE / 2, -IMAGE_SIZE / 2, IMAGE_SIZE / 2, IMAGE_SIZE / 2);
 	m_img.Draw();
 
 }
@@ -171,7 +170,7 @@ void Enemy01::HitCheck()
         printf("Player発見\n");
     }
 */
-	if (CollitionBase::CollisionCheckRectANDY(this, CharacterData::ePEffectLongAttack, 50.0f)) {
+	if (CollitionBase::CollisionCheckRectANDY(this, CharacterData::ePEffectLongAttack, 60.0f)) {
 		SOUND("punch-middle2")->Play();
 		m_hp -= 1;
 		m_state = Enemy01State::eDamage;
