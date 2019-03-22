ef0103cc7890234fa24d3d65733d9ef56fd741be
Kamata <18010503@anabuki-net.ne.jp>
2019-03-22 11:47:11 +0900

    a

--- Fukuyama/福山チーム/セーサク/GameProgramming/CBoss.cpp
+++ Fukuyama/福山チーム/セーサク/GameProgramming/CBoss.cpp
@@ -113,7 +113,7 @@ void CBoss::OnCollision(CCollider*p){
 			p->SetTexture(&Texture, 0, 64, 64, 0);
 			p->mPosition = mPosition;
 			CTaskManager::Get()->Add(p);
-			mHpBar.mHp -= 1.0f;
+			mHpBar.mHp -= 8.0f;
 
 			if (mHpBar.mHp <= 0.0f){
 
@@ -127,26 +127,26 @@ void CBoss::OnCollision(CCollider*p){
 
 		}
 
-		if (p->mpTask->mTaskTag == EPLAYERTANK){
-			CRectangle::SetTexture(&mTextImage3, 394, 552, -521, -426);
-			CExplosion*p = new CExplosion();
-			p->SetTexture(&Texture, 0, 64, 64, 0);
-			p->mPosition = mPosition;
-			CTaskManager::Get()->Add(p);
-			mHpBar.mHp -= 1.0f;
+		//if (p->mpTask->mTaskTag == EPLAYERTANK){
+		//	CRectangle::SetTexture(&mTextImage3, 394, 552, -521, -426);
+		//	CExplosion*p = new CExplosion();
+		//	p->SetTexture(&Texture, 0, 64, 64, 0);
+		//	p->mPosition = mPosition;
+		//	CTaskManager::Get()->Add(p);
+		//	mHpBar.mHp -= 1.0f;
 
-			if (mHpBar.mHp <= 0.0f){
-				CRectangle::SetTexture(&mTextImage3, -17, 160, -380, -191);
+		//	if (mHpBar.mHp <= 0.0f){
+		//		CRectangle::SetTexture(&mTextImage3, -17, 160, -380, -191);
 
-			}
-		}
+		//	}
+		//}
 		if (p->mpTask->mTaskTag == EPLAYERBULLET2){
 			CRectangle::SetTexture(&mTextImage3, 394, 552, -521, -426);
 			CExplosion*p = new CExplosion();
 			p->SetTexture(&Texture, 0, 64, 64, 0);
 			p->mPosition = mPosition;
 			CTaskManager::Get()->Add(p);
-			mHpBar.mHp -= 1.0f;
+			mHpBar.mHp -= 5.0f;
 
 			if (mHpBar.mHp <= 0.0f){
 				CRectangle::SetTexture(&mTextImage3, -17, 160, -380, -191);
@@ -161,9 +161,9 @@ void CBoss::OnCollision(CCollider*p){
 	}
 }
 void CBoss::OnCollision(CBoxCollider*p){
-	if (p->mpTask->mTaskTag == EPLAYERTANK){
-		mPosition = mPosition + mCollider->mAdjust;
-	}
+	//if (p->mpTask->mTaskTag == EPLAYERTANK){
+	//	mPosition = mPosition + mCollider->mAdjust;
+	//}
 }
 void CBoss::Render(){
 	mHpBar.Render();
